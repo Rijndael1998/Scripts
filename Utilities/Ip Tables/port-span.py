e9f67cd6ebb2451561db53a6fc6abf2a95bc964b
Rijndael <rijndael@baldy.ga>
2022-11-27 22:02:28 +0000

    removed forced parameter

--- Utilities/Ip Tables/port-span.py
+++ Utilities/Ip Tables/port-span.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     parser.add_argument("-L", "--locaddr", required=True, help="IP to \"listen\" on")
     parser.add_argument("-l", "--locport", required=True, help="The ascending sequential range of ports to \"listen\" on. Eg: 100-120 forwards ports 100 to 120 inclusive")
     parser.add_argument("-D", "--destaddr", required=True, help="Destination IP")
-    parser.add_argument("-d", "--destport", required=True, help="First destination port")
+    parser.add_argument("-d", "--destport", required=False, help="First destination port", default=None)
     parser.add_argument("-i", "--inter", help="Interface to \"listen\" on", default="enp8")
     parser.add_argument("-p", "--proto", help="The protocol. (tcp or udp)", default="tcp", choices=["tcp", "udp"])
     args = parser.parse_args()
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     pc = PortConstructor()
 
     i = 0
-    dest = int(args.destport)
+    dest = int(args.destport) if args.destport is not None else int(port[0])
     for locport in range(int(port[0]), int(port[1])+1):
         print(pc.construct(args.locaddr, locport, args.destaddr, dest + i, args.inter, args.proto))
         i += 1
